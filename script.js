3434d863f3c4a57210c159ee09f536f31c5ea1b5
dariawinnicka <dariawinnicka1278@gmail.com>
2024-08-14 22:40:16 +0100

    Initial commit

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 // --------------------------------- BODY ---------------------------------
 
 // Retrieved the DOM element representing the images.
-const websiteImage = document.querySelector(".img-software-web");
-const applicationImage = document.querySelector(".img-software-app");
+const imgWeb = document.querySelector(".img-software-web");
+const imgApp = document.querySelector(".img-software-app");
 
 // Initialised a variable to store the previous scroll position.
 let previousScrollPosition = 0;
@@ -21,21 +21,21 @@ window.addEventListener("scroll", function () {
   const documentHeight = document.documentElement.scrollHeight;
 
   // Calculated the tilt effect for the "image-software-website" based on its position relative to the viewport.
-  const websiteImageRect = websiteImage.getBoundingClientRect(); // Retrieved the position and dimensions of the element.
-  const websiteImageTop = websiteImageRect.top; // Calculated the distance from the top of the viewport to the element.
-  const websiteImageHeight = websiteImageRect.height; // Retrieved the height of the element.
-  const websiteImageVisibleHeight = Math.min(
+  const imgWebRect = imgWeb.getBoundingClientRect(); // Retrieved the position and dimensions of the element.
+  const imgWebTop = imgWebRect.top; // Calculated the distance from the top of the viewport to the element.
+  const imgWebHeight = imgWebRect.height; // Retrieved the height of the element.
+  const imgWebVisibleHeight = Math.min(
     viewportHeight,
-    websiteImageHeight + websiteImageTop
+    imgWebHeight + imgWebTop
   ); // Calculated the visible height within the viewport.
-  const websiteImageRelativePosition = viewportHeight - websiteImageTop; // Calculated the position relative to the viewport.
-  const websiteImageVisibilityPercent = Math.min(
-    Math.max(websiteImageRelativePosition / websiteImageVisibleHeight, 0),
+  const imgWebRelativePosition = viewportHeight - imgWebTop; // Calculated the position relative to the viewport.
+  const imgWebVisibilityPercent = Math.min(
+    Math.max(imgWebRelativePosition / imgWebVisibleHeight, 0),
     1
   ); // Calculated the percentage of visibility.
-  const websiteImageTiltAngle = 20 - 20 * websiteImageVisibilityPercent; // Calculated the tilt angle based on visibility.
+  const imgWebTiltAngle = 20 - 20 * imgWebVisibilityPercent; // Calculated the tilt angle based on visibility.
 
-  websiteImage.style.transform = `perspective(600px) rotateX(${websiteImageTiltAngle}deg)`; // Applied the tilt transformation.
+  imgWeb.style.transform = `perspective(600px) rotateX(${imgWebTiltAngle}deg)`; // Applied the tilt transformation.
 
   // Calculated the slide effect for the "image-software-application" based on the scroll position.
   const maxSlideDistance = 100; // Defined the maximum slide distance.
@@ -44,7 +44,7 @@ window.addEventListener("scroll", function () {
   const slideDistance = scrollPercent * maxSlideDistance; // Calculated the slide distance.
 
   // Applied the slide transformation to the "image-software-application".
-  applicationImage.style.transform = `translateX(-${slideDistance}px)`; // Applied the slide transformation.
+  imgApp.style.transform = `translateX(-${slideDistance}px)`; // Applied the slide transformation.
   // Updated the previous scroll position for reference in the next scroll event.
   previousScrollPosition = currentScrollPosition;
 });
@@ -101,4 +101,23 @@ document.addEventListener("DOMContentLoaded", function () {
   //   });
   // }
   // cookie-bar and cookie-settings open / close simultaneously
+});
+
+//
+//
+//
+//
+//
+//
+//
+//
+
+document.addEventListener("DOMContentLoaded", function () {
+  let arrDown = document.querySelector("#google-keyboard_arrow_down");
+
+  if (arrDown) {
+    arrDown.addEventListener("click", function () {
+      //
+    });
+  }
 });
